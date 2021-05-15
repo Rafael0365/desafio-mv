917bf170498ec56702ee1164a4d2a26d48026169
Rafael0365 <rafaelantonio0365@gmail.com>
2021-05-15 13:16:06 -0300

    bootstrap e principal.blade adicionado

---
+++ resources/views/layout/principal.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Café da manhã MV :)</h1>
+        @yield('conteudo')
+        </table>
+    </body>
+</html>
--- resources/views/participante.blade.php
+++ resources/views/participante.blade.php
@@ -1,18 +1,17 @@
-<html>
-    <body>
-        <h1>Café da manhã MV :)</h1>
-        <table>
-            <?php foreach($participantes as $c): ?>
-                <tr>
-                <td>Colaborador: <?= $c->nome?></td>
-                </tr>
-                <tr>
-                <td>CPF: <?= $c->cpf?></td>
-                </tr>
-                <tr>
-                <td>Prato que será trazido para o café da manhã: <?= $c->prato?></td>
-                </tr>
-            <?php endforeach ?>
-        </table>
-    </body>
-</html>
+@extends('layout.principal')
+@section('conteudo')
+    <h2>Listagem de colaboradores</h2>
+    <table>
+        @foreach($participantes as $c)
+            <tr>
+            <td>Colaborador: {{$c->nome}}</td>
+            </tr>
+            <tr>
+            <td>CPF: {{$c->cpf}}</td>
+            </tr>
+            <tr>
+            <td>Prato que será trazido para o café da manhã: {{$c->prato}}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
